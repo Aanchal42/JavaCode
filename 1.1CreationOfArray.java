84d29eec720828294255d2298e1c2863c2588c90
aanchal <anchaltiwari042@gmail.com>
2026-04-11 18:27:15 +0530

    Initial commit

---
+++ 1.1CreationOfArray.java
@@ -0,0 +1,15 @@
+class Demo{
+     public static void main(String[]args){
+          
+         int nums[] = new int[4];
+         nums[0] = 5;
+         nums[1] = 3;
+         nums[2] = 8;
+         nums[3] = 9;
+
+         for(int i=0 ; i<4 ; i++){
+            System.out.print(nums[i] + " ");
+         }
+
+     }
+}
