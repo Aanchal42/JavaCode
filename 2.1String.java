e9ce82eff5527e58e4bf9250be33c2282c434b96
aanchal <anchaltiwari042@gmail.com>
2026-04-11 23:09:34 +0530

    initial string

---
+++ 2.1String.java
@@ -0,0 +1,14 @@
+public class Demo {
+	public static void main(String[] args) 
+	{
+		 // String name = "Smith";
+		 
+		String name=new String("Smith");
+		System.out.println(name);
+		System.out.println(name.hashCode());
+		System.out.println("hello "+name);
+		System.out.println(name.concat("John"));
+
+		
+	}
+}
